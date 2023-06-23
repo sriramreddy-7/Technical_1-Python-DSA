4a7ed0cc85e217ba05573388148c7a7d803643ff
sriramreddy-7 <sriramreddykoonadi@gmail.com>
2023-06-23 15:37:26 +0530

    BST

--- Tree/Tree_ir.py
+++ Tree/Tree_ir.py
@@ -28,3 +28,18 @@ root.left.left=Node(4)
 root.left.right =Node(5)
 
 inOrder(root)
+'''
+every node is lesser than its parent if its lying at left side every node is greater than its parent if it is lying on right side'''
+'''50
+30                  
+10
+15
+16
+500
+300
+200
+1
+2
+3'''
+
+'''7 ,35,-4,-9,8,62,0,-25,25,16,17,77,-39,100'''
